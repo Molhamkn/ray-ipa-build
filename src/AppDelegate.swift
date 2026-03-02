79c97a23e9f50712dcfbe394a1be8a7bcef7deed
Molhamkn <molhamaws@gmail.com>
2026-03-02 10:27:27 -0500

    Add UI

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -17,6 +17,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var isConnected = false
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.rootViewController?.view.backgroundColor = .black
+        
+        let label = UILabel()
+        label.text = "Ray AI\nListening..."
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        window?.rootViewController?.view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: window!.rootViewController!.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: window!.rootViewController!.view.centerYAnchor)
+        ])
+        
+        window?.makeKeyAndVisible()
+        
         enableBackgroundModes()
         SFSpeechRecognizer.requestAuthorization { _ in }
         AVAudioSession.sharedInstance().requestRecordPermission { _ in }
